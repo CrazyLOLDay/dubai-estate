8c31ca32297514a969ecca0bd79b3f4d34b15834
CrazyLOLDay <archiref@yahoo.com>
2022-05-30 10:44:02 +0300

    Закончил мобильную версию

--- script/buttons.js
+++ script/buttons.js
@@ -1,10 +1,41 @@
 $(document).ready(function () {
-    $('.main_btn').on('click', function() {
+    $('.button_main, .button_main_mobile').on('click', function() {
        $('.success').removeClass('success').addClass('decline');
        $('.custom-checkbox:checked').prop('checked', false);
        $(this).parent().parent().fadeOut(1, function() {
-           $('.custom-checkbox:checked').prop('checked', false);
-           $(this).parent().parent().next().fadeIn(1);
+           $('.first_step').fadeIn(1);
        });
     });
+
+    $('.button_first').on('click', function() {
+        $(this).parent().parent().fadeOut(1, function() {
+            $('.second_step').fadeIn(1);
+        });
+    });
+
+    $('.button_second').on('click', function() {
+        $(this).parent().parent().fadeOut(1, function() {
+            $('.third_step').fadeIn(1);
+        });
+    });
+
+    $('.button_third').on('click', function() {
+        $(this).parent().parent().fadeOut(1, function() {
+            $('.fourth_step').fadeIn(1);
+        });
+    });
+
+    $('.button_fourth').on('click', function() {
+        $(this).parent().parent().fadeOut(1, function() {
+            $('.fifth_step').fadeIn(1);
+        });
+    });
+
+    $('.button_standard').on('click', function() {
+        $(this).parent().parent().fadeOut(1, function() {
+            $('.final_step').fadeIn(1);
+        });
+    });
+
+
 });
